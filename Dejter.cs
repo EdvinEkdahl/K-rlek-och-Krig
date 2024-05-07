58723dd62af1d8d2b0d74f3b98a5c2ac7834dcdf
EdvinEkdahl <edvin.ekdahl@icloud.com>
2024-05-07 13:42:08 +0200

    fgc

---
+++ Dejter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Dejter
+{
+    public void Alicia()
+    {
+        Console.WriteLine("Dejten är en kvinna som heter Alicia Datura, profilen säger att hon vill ha en partner som hon kan lita på som också litar på henne, hennes största hobby är att ta hand om hennes favoritblomma som är en Spikklubba. Hennes favoriträtt är kaktussallad. Hon jobbar bara kvällstid. 174 cm, långt rakt svart hår och smaragdgröna ögon.");
+    }
+
+    public void Robert()
+    {
+        Console.WriteLine("Dejten är en man som heter Robert “Byggare” Bob Spärrskaft Jr, profilen säger att han vill ha en aktiv partner som gillar byggarbete, hans största hobby är att laga saker. Han jobbar dagtid och har nästan ingen fritid så ni måste ha dejten under tiden han jobbar. Det står också att han bott i Bobsville, flyttade till Sunflower Valley och nu bor han i Fixham Harbour. Det står också att han älskar sin tvillingbror Tom “Djurälskare” Bob Spärrskaft. Robert 158 cm, flintis, bläck svarta ögon, går alltid runt med hård hatt och arbetskläder.\r\n");
+    }
+
+    public void Svea()
+    {
+        Console.WriteLine("Dejten är en kvinna som heter Svea Tove Åberg, profilen säger att hon letar efter en lugn partner och att hon hatar våld. Hennes största hobby är att titta på romantiska filmer. Hon är arbetslös. Hon är 166 cm, har kort brunt hår och chokladbruna ögon.");
+    }
+
+    public void Noah()
+    {
+        Console.WriteLine("Dejten är med en man som heter Noah Knutsson, profilen säger att han letar efter en partner som kan skydda honom från allt farligt i världen. Hans största hobby är att samla på vapen. Han jobbar dagtid på ett andra världskrigs museum. Det står också att han är väldigt lättskrämd. Han är 192 cm, kort blont hår med eld orangea ögon. \r\n");
+    }
+
+    public void Eloise()
+    {
+        Console.WriteLine("Dejten är med en kvinna som heter Eloise Vingarv, profilen säger att hon letar efter en partner som gillar att hitta på saker, spelar ingen roll vad det är. Hennes största hobby är bågskytte. Hon jobbar som advokat. Hon är 170 cm, har långt krulligt ljusbrunt hår och havs blåa ögon.\r\n");
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        Dejter dejter = new();
+        string playerChoose = "0";
+        while (playerChoose != "6")
+        {
+            //Skriver ut menyn.
+            Console.WriteLine("Välkomen till EoR Dejting.");
+            Console.WriteLine("Välj en av de här personerna att gå på dejt med (Genom att skriva in rätt nummer som står vid deras namn). " +
+                "\n1) Alicia Datura." +
+                "\n2) Robert “Byggare” Bob Spärrskaft Jr." +
+                "\n3) Svea Tove Åberg." +
+                "\n4) Noah Knutsson." +
+                "\n5) Eloise Vingarv." +
+                "\n6) Avsluta.");
+            playerChoose = Console.ReadLine();
+
+            Console.Clear();
+
+            switch (playerChoose)
+            {
+                case "1":
+                    dejter.Alicia();
+                    break;
+
+                case "2":
+                    dejter.Robert();
+                    break;
+
+                case "3":
+                    dejter.Svea();
+                    break;
+
+                case "4":
+                    dejter.Noah();
+                    break;
+
+                case "5":
+                    dejter.Eloise();
+                    break;
+
+                case "6":
+                    Console.WriteLine("Programmet avslutas.");
+                    return;
+
+                default:
+                    Console.WriteLine("Välj ett giltigt nummer.");
+                    break;
+            }
+        }
+            
+    }
+}
