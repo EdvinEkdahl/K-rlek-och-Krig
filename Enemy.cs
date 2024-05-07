754877700e29930dd5b9a43cd38bf0646e49cc05
EdvinEkdahl <edvin.ekdahl@icloud.com>
2024-05-07 12:28:12 +0200

    Some

--- Enemy.cs
+++ Enemy.cs
@@ -4,8 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Enemy
+class Enemy : Character
 {
+    public Enemy(string Name__, int Health__, int Streagnth__, int maxHealth__)
+    {
+        Name = Name__;
+        Health = Health__;
+        Streagnth = Streagnth__;
+        maxHealth = maxHealth__;
 
+        Health = maxHealth;
+    }
 }
 
--- Lista.cs
+++ Lista.cs
@@ -4,11 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class FIght
+class Lista
 {
-    public void takeDamage()
-    {
-        
-    }
+
 }
 
